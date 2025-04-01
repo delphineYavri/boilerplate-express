d8a14f43f37934e3a2715c63252566bf25067640
Delphine Erickson <delphine.erickson@yavr.io>
2025-04-01 16:14:07 +0200

    xxx

--- myApp.js
+++ myApp.js
@@ -3,10 +3,13 @@ let app = express();
 
 console.log("Hello World")
 
+app.get('/',function(get, res) {
+    res.sendFile(absolutePath = __dirname + '/views/index.html')}
+)
+
   app.get('/',function(get, res) {
         res.send('Hello Express')}
 )
-
 
 
 
